0438ad7161ef7300592643c078959bc48082818f
satyam <satyampathak67@gmail.com>
2023-12-22 20:33:03 +0530

    1.0.0

--- youtube/src/components/UserProfile.jsx
+++ youtube/src/components/UserProfile.jsx
@@ -1,7 +1,11 @@
-const UserProfile=({img})=>{
-    return(<>
-        <img className="" src={img} alt="profile picture"></img>
-        </>
+const UserProfile=({img,bio})=>{
+    return(
+    <div><div>
+        <img className="" src={img} alt="profile picture"></img></div>
+        <div>
+        <p>{bio}</p>
+        </div>
+        </div>
     )
 }
 export default UserProfile;
